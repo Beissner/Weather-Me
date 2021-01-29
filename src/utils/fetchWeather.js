12a5c75a2cecd0c111f3af76dfe6acdd4f620379
Emily Beissner <kebeissner@gmail.com>
2021-01-29 13:29:48 +0000

    more cleanup

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -17,17 +17,3 @@ export const fetchWeather = (long, lat) => {
 };
 
 
-/*
- fetch(
-            `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}&units=imperial`
-        )
-            .then(res => res.json())
-            .then(results => {
-                const description = results.weather[0].main;
-                setTemp(results.main.temp);
-                setLoading(false);
-                setWeather(description);
-                //this.setState({ temp: results.main.temp, loading: false, weatherCondition: description })
-            })
-            .catch((err) => console.log(err));
-*/
